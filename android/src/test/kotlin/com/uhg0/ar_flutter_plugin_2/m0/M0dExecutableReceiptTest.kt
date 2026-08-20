397da783985c5d213b69345589a24953a13eaa14
Lukas Jelinek <lukx19@gmail.com>
2026-08-21 00:43:02 +0200

    Generate immutable M0d native receipt

---
+++ android/src/test/kotlin/com/uhg0/ar_flutter_plugin_2/m0/M0dExecutableReceiptTest.kt
@@ -0,0 +1,10 @@
+package com.uhg0.ar_flutter_plugin_2.m0
+
+import org.junit.Test
+
+class M0dExecutableReceiptTest {
+    @Test
+    fun `T5 native renderer campaign emits an immutable executable receipt`() {
+        M0dT5ReceiptCampaign.execute()
+    }
+}
